f811991fc190bf41201b3a3e192f5e2cffd0f285
enforcer-a06 <enforcer.a06@gmail.com>
2024-11-28 23:35:51 +0800

    chore: 调整eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,38 @@
-import globals from 'globals';
+import { fixupConfigRules } from '@eslint/compat';
 import pluginJs from '@eslint/js';
-import pluginReact from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactJsx from 'eslint-plugin-react/configs/jsx-runtime.js';
+import react from 'eslint-plugin-react/configs/recommended.js';
+import globals from 'globals';
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
   {
-    ignores: ['*.js', 'dist/**/*', 'node_modules/**/*'],
-  },
-  {
-    files: ['**/*.{js,mjs,cjs,jsx}'],
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node,
         process: 'readonly',
       },
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-    },
-    rules: {
-      'react/react-in-jsx-scope': 'off',
     },
   },
+  pluginJs.configs.recommended,
+  ...fixupConfigRules([
+    {
+      ...react,
+      settings: {
+        react: { version: 'detect' },
+      },
+    },
+    reactJsx,
+  ]),
+  {
+    plugins: {
+      'react-hooks': reactHooks,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      // 'react/react-in-jsx-scope': 'off',
+    },
+  },
+  { ignores: ['dist/'] },
 ];
